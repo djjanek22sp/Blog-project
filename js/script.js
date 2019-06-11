84db9f0f96c94f18cae95a65645a325854dc9def
Daniel Janczyło <d.janczylo@gmail.com>
2019-06-11 18:47:04 +0200

    Add active class for this

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
   /* remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
@@ -12,7 +14,10 @@ function titleClickHandler(event){
   }
   /* add class 'active' to the clicked link */
 
+console.log('clickedElement:', clickedElement);
+this.classList.add('active')
   /* remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
