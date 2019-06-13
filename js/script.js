67f5a018b9827918b98e4c5555838283f98510b7
Daniel Janczyło <d.janczylo@gmail.com>
2019-06-13 19:29:18 +0200

    Add generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -56,18 +56,33 @@ function generateTitleLinks(){
 
   /* remove contents of titleList */
 
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
   /* for each article */
 
+  const articles = document.querySelectorAll(optArticleSelector);
+  for(let article of articles){
+
     /* get the article id */
+    const articleId = article.getAttribute('id');
 
-    /* find the title element */
+    /* find the title element */ /* get the title from the title element */
 
-    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* create HTML of the link */
 
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+
     /* insert link into titleList */
 
+    titleList.insertAdjacentHTML('beforeend', linkHTML);
+
+
+  }
 }
 
 generateTitleLinks();
